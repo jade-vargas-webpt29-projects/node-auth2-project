a01ea8c7111c1318d82322590a9a3dde6e2ff372
jadevargas92 <jadevargas92@gmail.com>
2021-10-03 20:47:13 -0500

    Completed auth token and middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,6 @@
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+const { JWT_SECRET } = require('../secrets'); // use this secret!
+const Users = require('../users/users-model');
+const jwt = require('jsonwebtoken');
 
 const restricted = (req, res, next) => {
   /*
@@ -16,9 +18,23 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
-}
 
-const only = role_name => (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return next({ status: 401, message: 'Token required' });
+  }
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      return next({ status: 401, message: 'Token invalid' });
+    }
+
+    req.decodedToken = decodedToken;
+    next();
+  });
+};
+
+const only = (role_name) => (req, res, next) => {
   /*
     If the user does not provide a token in the Authorization header with a role_name
     inside its payload matching the role_name passed to this function as its argument:
@@ -29,10 +45,14 @@ const only = role_name => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
-}
+  if (req.decodedToken.role === role_name) {
+    next();
+  } else {
+    next({ status: 403, message: 'This is not for you' });
+  }
+};
 
-
-const checkUsernameExists = (req, res, next) => {
+const checkUsernameExists = async (req, res, next) => {
   /*
     If the username in req.body does NOT exist in the database
     status 401
@@ -40,8 +60,14 @@ const checkUsernameExists = (req, res, next) => {
       "message": "Invalid credentials"
     }
   */
-}
-
+  const [usernameToFind] = await Users.findBy({ username: req.body.username });
+  if (!usernameToFind) {
+    next({ status: 422, message: 'Invalid credentials' });
+  } else {
+    req.user = usernameToFind;
+    next();
+  }
+};
 
 const validateRoleName = (req, res, next) => {
   /*
@@ -62,11 +88,24 @@ const validateRoleName = (req, res, next) => {
       "message": "Role name can not be longer than 32 chars"
     }
   */
-}
+  const { role_name } = req.body;
+
+  if (!role_name || role_name.trim() === '') {
+    req.body.role_name = 'student';
+  } else if (role_name.trim() === 'admin') {
+    res.status(422).json({ message: 'Role name can not be admin' });
+  } else if (role_name.trim().length > 32) {
+    res
+      .status(422)
+      .json({ message: 'Role name can not be longer than 32 chars' });
+  } else {
+    next();
+  }
+};
 
 module.exports = {
   restricted,
   checkUsernameExists,
   validateRoleName,
   only,
-}
+};
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,10 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+const { JWT_SECRET } = require('../secrets'); // use this secret!
+const bcrypt = require('bcryptjs');
+const Users = require('../users/users-model');
 
-router.post("/register", validateRoleName, (req, res, next) => {
+router.post('/register', validateRoleName, (req, res, next) => {
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
 
@@ -14,10 +16,24 @@ router.post("/register", validateRoleName, (req, res, next) => {
       "role_name": "angel"
     }
    */
+  let user = req.body;
+
+  // bcrypting the password before saving
+  const rounds = process.env.BCRYPT_ROUNDS || 8; // 2 ^ 8
+  const hash = bcrypt.hashSync(user.password, rounds);
+
+  // NEVER SAVE the plain text password in the db
+
+  user.password = hash;
+
+  Users.add(user)
+    .then((saved) => {
+      res.status(201).json({ message: `Great to have you, ${saved.username}` });
+    })
+    .catch(next);
 });
 
-
-router.post("/login", checkUsernameExists, (req, res, next) => {
+router.post('/login', checkUsernameExists, (req, res, next) => {
   /**
     [POST] /api/auth/login { "username": "sue", "password": "1234" }
 
@@ -37,6 +53,31 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
       "role_name": "admin" // the role of the authenticated user
     }
    */
+  let { password } = req.body;
+  Users.findBy({ username }).then(([user]) => {
+    if (bcrypt.compareSync(password, user.password)) {
+      const token = tokenBuilder(user);
+      res.status(200).json({
+        message: `${user.username} is back`,
+        token,
+      });
+    } else {
+      next({ status: 401, message: 'Invalid Credentials' });
+    }
+  });
+
+  function tokenBuilder(user) {
+    const payload = {
+      sub: user.id,
+      username: user.username,
+      role: user.role_name,
+    };
+    const options = {
+      expiresIn: '1d',
+    };
+
+    return jwt.sign(payload, JWT_SECRET, options);
+  }
 });
 
 module.exports = router;
